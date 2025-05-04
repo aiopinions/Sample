5dbb0306e64d36910364d3f486427fceac066cb3
aiopinions <161578121+aiopinions@users.noreply.github.com>
2025-05-04 12:55:32 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,3 +2,7 @@ const sample_root = document.getElementById('sample_root');
 sample_root.addEventListener('load', () => {
   console.log('page is loaded');
 });
+const loginbtn = document.createElement('button');
+loginbtn.addEventListener('click', () => {
+  window.href('/ello');
+}
