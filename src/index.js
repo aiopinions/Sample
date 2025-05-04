15a1bf8878809eb85ca5705b3fe1be55f94cff36
aiopinions <161578121+aiopinions@users.noreply.github.com>
2025-05-04 12:57:18 -0400

    Delete src/index.js

--- src/index.js
+++
@@ -1,9 +0,0 @@
-const sample_root = document.getElementById('sample_root');
-sample_root.addEventListener('load', () => {
-  console.log('page is loaded');
-});
-const loginbtn = document.createElement('button');
-loginbtn.addEventListener('click', () => {
-  window.href('/ello');
-}
-sample_root.appendChild(loginbtn);
